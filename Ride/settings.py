594ec6148259589c6103341e7f79fc24c2e4579e
LRomero2 <lauren.romero@outlook.com>
2021-02-09 08:05:23 +0000

    Added iport os to setting.py as that was missing.

--- Ride/settings.py
+++ Ride/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os 
+
 
 from pathlib import Path
 
