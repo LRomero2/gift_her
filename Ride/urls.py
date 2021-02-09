75a66c2e5d823dd176f7d38b68c2f153f65d73cf
LRomero2 <lauren.romero@outlook.com>
2021-02-09 09:25:10 +0000

    Added products page.

--- Ride/urls.py
+++ Ride/urls.py
@@ -1,7 +1,6 @@
 """Ride URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,9 +17,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
