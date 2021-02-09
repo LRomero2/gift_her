d4f5cf16fbe11c4c8f8653bfdfb3df8acd0d9472
LRomero2 <lauren.romero@outlook.com>
2021-02-09 09:15:42 +0000

    Customized admin with products and categories.

--- products/models.py
+++ products/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 
-
-
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
