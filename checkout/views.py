46da8b1cb4977badb02c57eef60837c48d83ecd6
LRomero2 <lauren.romero@outlook.com>
2021-02-16 12:45:02 +0000

    Added more to stripe payments. Working on fixing bug that appears due to api's.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
 
@@ -34,7 +35,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51ILS03CBaLtR0okmu8jUFiQYG3lsboRxqbMeHMR1SIhR7rCPApZ2zWweJLapp0RhhiASEoL1bAJCWVTg8KFTVX1H009z7lLs3g',
+        'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
     }
 
