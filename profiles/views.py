26cdf841f33eb5ffceeb0797baf737a7bdad97f2
LRomero2 <lauren.romero@outlook.com>
2021-02-22 10:31:06 +0000

    Added order history and updated views in checkout. Enabled profile user to save details to their profile.

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
-from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
-
 from .models import UserProfile
 from .forms import UserProfileForm
 
