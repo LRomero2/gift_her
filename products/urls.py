9ee95e93e2b8b108ac3a95446e16c93e9e1dabb2
LRomero2 <lauren.romero@outlook.com>
2021-02-23 17:11:13 +0000

    For product admin, created view, url and template.

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+  from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
