e4998bf4d499359ba7da94b9f1c1be0ad41b9d8c
LRomero2 <lauren.romero@outlook.com>
2021-03-23 21:34:47 +0000

    Added new style to the home page and styled thankyou page to display content.

--- contact_us/urls.py
+++ contact_us/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.secure_message, name='contact_us'),
-    path('contact/', views.faqs, name='faqs'),
-    path('contact/', views.about, name='about'),
-    path('contact/', views.thankyou_page, name='thankyou_page'),
+    path('faq/', views.faqs, name='faqs'),
+    path('about/', views.about, name='about'),
+    path('thankyou/', views.thankyou_page, name='thankyou_page'),
 ]
