53865464548bfe6652a0e2fcd509373baf3aefe4
LRomero2 <lauren.romero@outlook.com>
2021-03-31 16:44:26 +0000

    Testing deployment again.

--- gift_her/settings.py
+++ gift_her/settings.py
@@ -176,7 +176,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-     # Cache control
+    # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
