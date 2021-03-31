2dae1d5ca4ada7e3afa2b0974926846aae33046a
LRomero2 <lauren.romero@outlook.com>
2021-03-31 16:43:42 +0000

    Made settings code pep8 compliant.

--- gift_her/settings.py
+++ gift_her/settings.py
@@ -181,7 +181,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'gift-her'
     AWS_S3_REGION_NAME = 'eu-west-2'
