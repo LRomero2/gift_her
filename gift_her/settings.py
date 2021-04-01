dff4d2dabffa3177f0b78a92749251abe9f34b88
LRomero2 <lauren.romero@outlook.com>
2021-04-01 21:02:35 +0000

    Changed debug value back to development.

--- gift_her/settings.py
+++ gift_her/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gift-her.herokuapp.com', 'localhost']
 
